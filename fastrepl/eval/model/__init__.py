2dd0209cb68d532282cbd3cfa68bc7f1eb507504
yujong-lee <yujonglee@g.skku.edu>
2023-08-21 21:37:28 +0900

    Pass CI

--- fastrepl/eval/model/__init__.py
+++ fastrepl/eval/model/__init__.py
@@ -1,9 +1,7 @@
-from fastrepl.eval.model.llm_cot import LLMChainOfThought
 from fastrepl.eval.model.llm_classify import LLMClassifier
 from fastrepl.eval.model.llm_cot_classify import LLMChainOfThoughtClassifier
 
 __all__ = [
-    "LLMChainOfThought",
     "LLMClassifier",
     "LLMChainOfThoughtClassifier",
 ]
