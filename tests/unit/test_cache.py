a678e90346e72891ddb594c326dc15f7961fb929
yujong-lee <yujonglee@g.skku.edu>
2023-08-24 16:06:22 +0900

    Enhance testing

--- tests/unit/test_cache.py
+++ tests/unit/test_cache.py
@@ -2,14 +2,13 @@ import pytest
 from _pytest.fixtures import FixtureRequest
 
 import json
-from sqlalchemy import create_engine
 
 import fastrepl
-from fastrepl.run.cache import SQLAlchemyCache
+from fastrepl.run.cache import SQLiteCache
 
 
-def get_sqlite_cache() -> SQLAlchemyCache:
-    return SQLAlchemyCache(engine=create_engine("sqlite://"))
+def get_sqlite_cache() -> SQLiteCache:
+    return SQLiteCache(path=":memory:")
 
 
 CACHE_OPTIONS = [
@@ -21,41 +20,49 @@ CACHE_OPTIONS = [
 def set_cache_and_teardown(request: FixtureRequest):
     cache_instance = request.param
     fastrepl.cache = cache_instance()
-    if fastrepl.cache:
-        fastrepl.cache.clear()
-    else:
-        raise ValueError("Cache not set. This should never happen.")
 
+    # fmt: off
+    if fastrepl.cache: fastrepl.cache.clear()
     yield
-
-    if fastrepl.cache:
-        fastrepl.cache.clear()
-    else:
-        raise ValueError("Cache not set. This should never happen.")
+    if fastrepl.cache: fastrepl.cache.clear()
+    # fmt: on
 
 
 class TestCache:
     def test_basic(self):
-        if fastrepl.cache:
-            fastrepl.cache.update("model", "prompt", "response")
-            assert fastrepl.cache.lookup("model", "prompt") == "response"
-            assert fastrepl.cache.lookup("model", "prompt2") is None
+        assert fastrepl.cache is not None
 
-            fastrepl.cache.clear()
-            assert fastrepl.cache.lookup("model", "prompt") is None
-        else:
-            raise ValueError("Cache not set. This should never happen.")
+        fastrepl.cache.update("model", "prompt", "response")
+        assert fastrepl.cache.lookup("model", "prompt") == "response"
+        assert fastrepl.cache.lookup("model", "prompt2") is None
+
+        fastrepl.cache.clear()
+        assert fastrepl.cache.lookup("model", "prompt") is None
 
     def test_json(self):
-        if fastrepl.cache:
-            msgs = [
-                {
-                    "role": "user",
-                    "text": "Hello, how are you?",
-                }
-            ]
-            res = {"text": "1"}
-            fastrepl.cache.update("model", json.dumps(msgs), json.dumps(res))
-            assert fastrepl.cache.lookup("model", json.dumps(msgs)) == json.dumps(res)
-        else:
-            raise ValueError("Cache not set. This should never happen.")
+        assert fastrepl.cache is not None
+
+        msgs = [
+            {
+                "role": "user",
+                "text": "Hello, how are you?",
+            }
+        ]
+        res = {"text": "1"}
+        fastrepl.cache.update("model", json.dumps(msgs), json.dumps(res))
+        assert fastrepl.cache.lookup("model", json.dumps(msgs)) == json.dumps(res)
+
+    def test_multiple(self):
+        assert fastrepl.cache is not None
+
+        fastrepl.cache.update("model1", "prompt", "response1")
+        fastrepl.cache.update("model1", "prompt", "response2")
+        fastrepl.cache.update("model1", "prompt", "response3")
+        assert fastrepl.cache.lookup("model1", "prompt") == "response3"
+
+        fastrepl.cache.update("model2", "prompt", "response4")
+        fastrepl.cache.update("model2", "prompt", "response5")
+        fastrepl.cache.update("model2", "prompt", "response6")
+        assert fastrepl.cache.lookup("model2", "prompt") == "response6"
+
+        assert fastrepl.cache.lookup("model1", "prompt") == "response3"
