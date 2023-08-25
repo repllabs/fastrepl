1d1680f528976fe656ac926bc836de9f751e8bdf
yujong-lee <yujonglee@g.skku.edu>
2023-08-25 17:59:35 +0900

    Now we run evaluator inside REPL

--- fastrepl/eval/evaluator.py
+++ fastrepl/eval/evaluator.py
@@ -1,60 +1,21 @@
 import functools
 from typing import List
-from multiprocessing.pool import ThreadPool
-
-from rich.progress import Progress
-from datasets import Dataset
 
 from fastrepl.eval.model.base import BaseModelEval
-from fastrepl.utils import getenv
-
-NUM_THREADS = getenv("NUM_THREADS", 8)
 
 
 # TODO: we assume only model-eval here.
 class Evaluator:
-    __slots__ = [
-        "dataset",
-        "pipeline",
-        "input_feature",
-        "prediction_feature",
-        "initial_context",
-    ]
+    __slots__ = ["pipeline"]
 
-    def __init__(
-        self,
-        dataset: Dataset,
-        pipeline: List[BaseModelEval],
-        input_feature: str = "input",
-        prediction_feature: str = "prediction",
-    ) -> None:
-        if input_feature not in dataset.features:
-            raise ValueError(f"input feature {input_feature!r} not in dataset")
-
-        self.dataset = dataset
+    def __init__(self, pipeline: List[BaseModelEval]) -> None:
+        if len(pipeline) == 0:
+            raise ValueError("Pipeline cannot be empty")
         self.pipeline = pipeline
-        self.input_feature = input_feature
-        self.prediction_feature = prediction_feature
 
-    def _run_evals(self, input: str) -> str:
+    def run(self, sample: str, initial_context="") -> str:
         return functools.reduce(
-            lambda previous, eval: eval.compute(input, previous),
+            lambda context, eval: eval.compute(sample, context),
             self.pipeline,
-            self.initial_context,
+            initial_context,
         )
-
-    def run(self, context="") -> Dataset:
-        self.initial_context = context
-
-        results = []
-
-        with Progress() as progress:
-            inputs = self.dataset[self.input_feature]
-            task = progress.add_task("[cyan]Processing...", total=len(inputs))
-
-            with ThreadPool(NUM_THREADS) as pool:
-                for result in pool.imap(self._run_evals, inputs):
-                    results.append(result)
-                    progress.update(task, advance=1, refresh=True)
-
-        return self.dataset.add_column(self.prediction_feature, results)
--- fastrepl/loop/repl.py
+++ fastrepl/loop/repl.py
@@ -3,57 +3,40 @@
 
 from typing import List
 from contextlib import ContextDecorator
-import time
-import random
 import importlib.metadata
 
-from rich import box
-from rich.live import Live
-from rich.table import Table
-
-from fastrepl.utils import get_cuid
-from fastrepl.context import REPLContext
+from rich.progress import Progress
+from multiprocessing.pool import ThreadPool
 
 from fastrepl.eval import Evaluator
+from fastrepl.utils import getenv, get_cuid
+from fastrepl.context import REPLContext
 
 
-def generate_table() -> Table:
-    table = Table(title="fastrepl", box=box.MINIMAL)
-    table.add_column("ID")
-    table.add_column("Value")
-    table.add_column("Status")
-
-    for row in range(random.randint(2, 6)):
-        value = random.random() * 100
-        table.add_row(
-            f"{row}", f"{value:3.2f}", "[red]ERROR" if value < 50 else "[green]SUCCESS"
-        )
-    return table
-
-
+NUM_THREADS = getenv("NUM_THREADS", 8)
 DEFAULT_INFO = {"fastrepl": importlib.metadata.version("fastrepl")}
 
 
 class REPLController:
-    __slots__ = ("id", "info", "_evaluators", "_display")
+    __slots__ = ("id", "info", "_evaluator", "_display")
 
     def __init__(self):
         self.id = get_cuid()
         self.info = DEFAULT_INFO
 
-    def _set_display(self, live: Live):
-        self._display = live
+    def set_evaluator(self, evaluator: Evaluator):
+        self._evaluator = evaluator
 
-    def refresh(self):
-        self._display.update(generate_table())
+    def eval(self, inputs: List[str]) -> List[str]:
+        ret = []
+        with Progress() as progress:
+            task = progress.add_task("[cyan]Processing...", total=len(inputs))
 
-    def set_evaluators(self, evaluators: List[Evaluator]):
-        self._evaluators = evaluators
-
-    def run(self):
-        for eval in self._evaluators:
-            eval.run()  # TODO: We need to refresh it inside eval. should we pass it inside?
-            # This might be due to design flaw. REPL controller is not actually controlling anything.
+            with ThreadPool(NUM_THREADS) as pool:
+                for result in pool.imap(self._evaluator.run, inputs):
+                    ret.append(result)
+                    progress.update(task, advance=1, refresh=True)
+        return ret
 
 
 class REPL(ContextDecorator):
@@ -63,11 +46,8 @@ class REPL(ContextDecorator):
         self.controller = REPLController()
 
     def __enter__(self) -> REPLController:
-        live = Live(generate_table(), refresh_per_second=4).__enter__()
-        self.controller._set_display(live)
         return self.controller
 
     def __exit__(self, *args):
         REPLContext.reset()
-        self.controller._display.__exit__(*args)
         self.controller = None
