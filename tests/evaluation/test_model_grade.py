63aa02ebff498e5d3b39098d6a498ea48acbf94c
yujong-lee <yujonglee@g.skku.edu>
2023-08-26 12:50:41 +0900

    Update test

--- tests/evaluation/test_model_grade.py
+++ tests/evaluation/test_model_grade.py
@@ -1,5 +1,7 @@
 import pytest
+from datasets import Dataset
 
+from fastrepl.eval.metric import load_metric
 from fastrepl.eval.model import (
     LLMClassifier,
     LLMChainOfThought,
@@ -7,12 +9,8 @@ from fastrepl.eval.model import (
 )
 
 
-@pytest.mark.fastrepl
-def test_try_pytest_plugin():
-    assert True
-
-
 class TestClassifier:
+    @pytest.mark.fastrepl
     def test_single_classifier(self):
         labels = {
             "POSITIVE": "Given text is actually positive",
@@ -20,12 +18,33 @@ class TestClassifier:
         }
         eval = LLMClassifier(
             model="gpt-3.5-turbo",
+            context="You will get a input text by a liar. Take it as the opposite.",
             labels=labels,
         )
 
-        assert eval.compute("What a great day!") in labels.keys()
-        assert eval.compute("What a bad day!") in labels.keys()
+        tc = Dataset.from_dict(
+            {
+                "input": [
+                    "What a great day!",
+                    "What a bad day!",
+                    "I am so happy.",
+                    "I am so sad.",
+                ],
+                "reference": [
+                    "POSITIVE",
+                    "NEGATIVE",
+                    "NEGATIVE",
+                    "POSITIVE",
+                ],
+            }
+        )
 
+        predictions, references = [eval.compute(input) for input in tc["input"]]
+        references = tc["reference"]
+
+        assert load_metric("accuracy").compute(predictions, references) > 0.5
+
+    @pytest.mark.fastrepl
     def test_cot_with_classifier(self):
         input = "What a great day!"
         labels = {
@@ -45,10 +64,28 @@ class TestClassifier:
             ),
         ]
 
+        tc = Dataset.from_dict(
+            {
+                "input": [
+                    "What a great day!",
+                    "What a bad day!",
+                    "I am so happy.",
+                    "I am so sad.",
+                ],
+                "reference": [
+                    "POSITIVE",
+                    "NEGATIVE",
+                    "NEGATIVE",
+                    "POSITIVE",
+                ],
+            }
+        )
+
         thought = pipeline[0].compute(input)
         answer = pipeline[1].compute(input, context=thought)
         assert answer in labels.keys()
 
+    @pytest.mark.fastrepl
     def test_cot_and_classify(self):
         labels = {
             "POSITIVE": "Given text is actually positive",
