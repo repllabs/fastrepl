92894a2b4a3fbb3e2384ff3e7971457dfbc940e1
yujong-lee <yujonglee@g.skku.edu>
2023-08-27 11:55:25 +0900

    Initial implementation of runner

--- fastrepl/__init__.py
+++ fastrepl/__init__.py
@@ -3,6 +3,10 @@ from fastrepl.cache import llm_cache
 
 import fastrepl.eval as eval
 from fastrepl.polish import Updatable
+from fastrepl.runner import (
+    LocalRunner,
+    RemoteRunner,
+)
 
 from fastrepl.errors import (
     InvalidStatusError,
--- fastrepl/repl/__init__.py
+++ fastrepl/repl/__init__.py
@@ -6,6 +6,11 @@ import fastrepl.eval as eval
 import fastrepl.cache as cache
 from fastrepl.cache import llm_cache
 
+from fastrepl.runner import (
+    LocalRunnerREPL as LocalRunner,
+    RemoteRunnerREPL as RemoteRunner,
+)
+
 from fastrepl.errors import (
     InvalidStatusError,
 )
