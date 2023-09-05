3ce6bf01bf3954feaa54754dcaea64696199cdaa
yujong-lee <yujonglee@g.skku.edu>
2023-09-05 10:28:53 +0900

    Add number util

--- fastrepl/utils/string.py
+++ fastrepl/utils/string.py
@@ -1,3 +1,6 @@
+from typing import Optional, Union
+
+
 def truncate(text: str, max: int) -> str:
     if len(text) <= max:
         return text
@@ -6,3 +9,14 @@ def truncate(text: str, max: int) -> str:
         raise ValueError("max must be at least 4")
 
     return text[: max - 3] + "..."
+
+
+def number(s: str) -> Optional[Union[int, float]]:
+    s = s.strip()
+    try:
+        return int(s)
+    except ValueError:
+        try:
+            return float(s)
+        except ValueError:
+            return None
