f72fbc933a141a659d2cb7b5cfbb30e886381fcf
yujong-lee <yujonglee@g.skku.edu>
2023-09-23 20:57:14 +0900

    Add method overloading

--- fastrepl/dataset.py
+++ fastrepl/dataset.py
@@ -1,4 +1,4 @@
-from typing import Literal, List
+from typing import Literal, Optional, List, overload
 
 import time
 import requests
@@ -9,43 +9,62 @@ from fastrepl.utils import console
 
 
 class Dataset:
+    @overload
     @classmethod
     def generate_from(
-        cls, kind: Literal["question"], source: List[str]
+        cls, kind: Literal["question"], *, job_id: str
     ) -> HuggingfaceDataset:
-        assert kind == "question"
+        ...
 
-        new_res = requests.post(
-            f"{fastrepl.api_base}/question/generate/new",
-            headers={
-                "Content-Type": "application/json",
-                "Authorization": f"Bearer {fastrepl.api_key}",
-            },
-            json={"texts": source},
-        )
+    @overload
+    @classmethod
+    def generate_from(
+        cls, kind: Literal["question"], *, source: List[str]
+    ) -> HuggingfaceDataset:
+        ...
 
-        if new_res.status_code != 200:
-            raise Exception(new_res.json())
+    @classmethod
+    def generate_from(
+        cls,
+        kind: Literal["question"],
+        *,
+        source: Optional[List[str]] = None,
+        job_id: Optional[str] = None,
+    ) -> HuggingfaceDataset:
+        assert kind == "question"  # TODO
 
-        id = new_res.json()["id"]
-
-        def get() -> requests.Response:
-            return requests.get(
-                f"{fastrepl.api_base}/question/generate/result/{id}",
+        if job_id is None:
+            new_res = requests.post(
+                f"{fastrepl.api_base}/question/generate/new",
                 headers={
                     "Content-Type": "application/json",
                     "Authorization": f"Bearer {fastrepl.api_key}",
                 },
+                json={"texts": source},
             )
 
-        with console.status("Waiting for result..."):
+            if new_res.status_code != 200:
+                raise Exception(new_res.json())
+
+            job_id = new_res.json()["id"]
+
+        with console.status(f"[cyan3]Waiting for job '{job_id}' to finish..."):
             while True:
-                res = get()
+                res = requests.get(
+                    f"{fastrepl.api_base}/question/generate/result/{job_id}",
+                    headers={
+                        "Content-Type": "application/json",
+                        "Authorization": f"Bearer {fastrepl.api_key}",
+                    },
+                )
+
+                if res.status_code not in [200, 202]:
+                    raise Exception(res.json())
+
                 if res.status_code == 202:
                     time.sleep(0.5)
                     continue
 
-                if res.status_code == 200:
-                    return res.json()
-
-                raise Exception(res.json())
+                return HuggingfaceDataset.from_dict(
+                    {"question": res.json()},
+                )
