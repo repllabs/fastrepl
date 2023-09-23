e4276f0a6d04ee8f7e19ea690e45c355ce9e91b2
yujong-lee <yujonglee@g.skku.edu>
2023-09-23 20:26:25 +0900

    Add result pulling

--- fastrepl/dataset.py
+++ fastrepl/dataset.py
@@ -1,9 +1,11 @@
 from typing import Literal, List
 
+import time
 import requests
 from datasets import Dataset as HuggingfaceDataset
 
 import fastrepl
+from fastrepl.utils import console
 
 
 class Dataset:
@@ -13,15 +15,37 @@ class Dataset:
     ) -> HuggingfaceDataset:
         assert kind == "question"
 
-        res = requests.post(
+        new_res = requests.post(
             f"{fastrepl.api_base}/question/generate/new",
             headers={
                 "Content-Type": "application/json",
                 "Authorization": f"Bearer {fastrepl.api_key}",
             },
-            json={"texts": ["hello", "world"]},
+            json={"texts": source},
         )
 
-        id = res.json()["id"]
+        if new_res.status_code != 200:
+            raise Exception(new_res.json())
 
-        return id
+        id = new_res.json()["id"]
+
+        def get() -> requests.Response:
+            return requests.get(
+                f"{fastrepl.api_base}/question/generate/result/{id}",
+                headers={
+                    "Content-Type": "application/json",
+                    "Authorization": f"Bearer {fastrepl.api_key}",
+                },
+            )
+
+        with console.status("Waiting for result..."):
+            while True:
+                res = get()
+                if res.status_code == 202:
+                    time.sleep(0.5)
+                    continue
+
+                if res.status_code == 200:
+                    return res.json()
+
+                raise Exception(res.json())
