cff6963ea5e31346fa52afb35b112749266f81c0
yujong-lee <yujonglee@g.skku.edu>
2023-10-09 15:12:13 -0700

    bump v

--- fastrepl/__init__.py
+++ fastrepl/__init__.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-__version__ = "0.0.16"
+__version__ = "0.0.17"
 
 api_base: Optional[str] = "https://yujonglee--fastrepl-api.modal.run"  # TODO
 api_key: Optional[str] = None
